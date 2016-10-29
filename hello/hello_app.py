ffa8940fd68a15358d86d1d1d5ca964faa486ecf
marziahkptm <percipience0107@yahoo.com>
2016-10-28 20:20:10 -0700

    Update hello_app.py

--- hello/hello_app.py
+++ hello/hello_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, I love continuous delivery a lot!"
+    return "Hello World, I love continuous delivery a lot! I love SCM class very much"
 
 if __name__ == "__main__":
     app.run()
